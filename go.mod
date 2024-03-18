35fb7e7b9b9df359cdd49e24745e7749c0cc8116
zheng peng <pengzheng@huishoubao.com.cn>
2024-03-18 08:57:21 +0800

    first

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/suifengpiao14/goscript
+
+go 1.21.0
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/cast v1.6.0
+	github.com/stretchr/testify v1.9.0
+	github.com/syyongx/php2go v0.9.8
+	github.com/tidwall/gjson v1.17.1
+	github.com/tidwall/sjson v1.2.5
+	github.com/traefik/yaegi v0.16.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ script.go
@@ -0,0 +1,38 @@
+package goscript
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/suifengpiao14/goscript/yaegi"
+)
+
+const (
+	SCRIPT_LANGUAGE_GO = yaegi.SCRIPT_LANGUAGE_GO
+)
+
+type Script struct {
+	Language string `json:"language"`
+	Code     string `json:"code"`
+}
+
+type ScriptI interface {
+	Language() string
+	Compile() (err error)
+	Run(script string) (out string, err error)
+	CallFuncScript(funcName string, input string) (callFuncScript string) //最终调用函数代码
+}
+
+func NewScriptEngine(language string) (scriptI ScriptI, err error) {
+	m := map[string]func() ScriptI{
+		SCRIPT_LANGUAGE_GO: func() ScriptI {
+			return yaegi.NewScriptGo()
+		},
+	}
+	fn, ok := m[strings.ToLower(language)]
+	if !ok {
+		err = errors.Errorf("not found script engine by language:%s", language)
+		return nil, err
+	}
+	return fn(), nil
+}
